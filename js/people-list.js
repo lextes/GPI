39a24ee88dd0d2b67aa77ec65717f0825eafa171
lextes <forza1708@gmail.com>
2017-05-06 10:30:17 -0500

    carga inicial

---
+++ js/people-list.js
@@ -0,0 +1,86 @@
+angular.module('project').value('projectListValue', [
+  {
+    name: 'Maggie',
+    firstName:'Coleman',
+    secondName:'Trump',
+    birthday: '5/02/1970',
+
+    car1: 'Nissan',
+    model: 'Tsuru',
+    year: '2010',
+    color: 'white'
+  },
+  {
+    name: 'Houston',
+    firstName:'Have',
+    secondName:'Trouble',
+    birthday: '13/12/2000',
+
+    car1: 'Volkswagen',
+    model: 'sedan',
+    year: '1958',
+    color: 'white'
+
+    car2: 'Volkswagen',
+    model: 'combi',
+    year: '1945',
+    color: 'pink',
+
+    car3: 'Nissan',
+    model: 'sentra',
+    year: '2010',
+    color: 'grey'
+  },
+  {
+    name: 'Stephanie',
+    firstName:'Calamargo',
+    secondName:'Tutis',
+    birthday: '20/10/1980',
+
+    car1: 'Mercedes',
+    model: 'sedan',
+    year: '2010',
+    color: 'white'
+
+    car2: 'Nissan',
+    model: 'sentra',
+    year: '2017',
+    color: 'grey',
+
+    car3: 'General motors',
+    model: 'lobo',
+    year: '2000',
+    color: 'red',
+
+    car4: 'Nissan',
+    model: 'sentra',
+    year: '2010',
+    color: 'grey',
+
+    car5: 'Nissan',
+    model: 'sentra',
+    year: '2010',
+    color: 'grey'
+  },
+  {
+    name: 'Hope',
+    firstName:'Resendiz',
+    secondName:'Icaro',
+    birthday: '7/8/1990',
+
+    car1: 'Volkswagen',
+    model: 'jetta',
+    year: '1990',
+    color: 'red'
+
+    car2: 'Nissan',
+    model: 'Altima',
+    year: '2003',
+    color: 'white',
+
+    car3: 'Audi',
+    model: 'Z38',
+    year: '2010',
+    color: 'black'
+  },
+])
